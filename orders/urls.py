b6b4a8830735c240800ac869a547db127d031d15
usmanfcit <usmanaltaf.work@gmail.com>
2024-08-20 14:37:04 +0500

    Class views + Multiple orders completed

--- orders/urls.py
+++ orders/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path("delete-from-cart/<int:pk>/", views.DeleteProductFromCart.as_view(), name="delete_product_from_cart"),
     path("create-support-ticket/<int:order_id>/", views.CreateSupportTicket.as_view(), name="create_support_ticket"),
     path("show-cart/", views.ShowCart.as_view(), name="show_cart"),
+    path("show-orders/", views.ShowOrders.as_view(), name="show_orders"),
     path("show-ticket-form/<int:pk>/", views.ShowTicketForm.as_view(), name="show_ticket_form"),
     path("list-support-tickets/", views.ListSupportTickets.as_view(), name="list_support_tickets"),
+    path("finalize-order/<int:order_id>", views.FinalizeOrder.as_view(), name="finalize_order"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -7,6 +7,16 @@ from django.views.generic.list import ListView
 
 from orders.models import Order, OrderItem, SupportTicket
 from products.models import Product
+from .status_choices import OrderStatusChoices
+
+
+class FinalizeOrder(View):
+    def post(self, request, *args, **kwargs):
+        order_id = kwargs["order_id"]
+        Order.objects.filter(id=order_id).update(
+            order_status=OrderStatusChoices.ORDER_PLACED
+        )
+        return redirect("orders:show_orders")
 
 
 class AddToCart(View):
@@ -16,6 +26,7 @@ class AddToCart(View):
         product = Product.objects.get(id=product_id)
         order, created = Order.objects.get_or_create(
             user=current_user,
+            order_status=OrderStatusChoices.IN_CART
         )
         order_item, order_item_created = OrderItem.objects.get_or_create(order=order, product=product)
 
@@ -38,13 +49,22 @@ class DeleteProductFromCart(DeleteView):
     success_url = reverse_lazy("orders:show_cart")
 
 
+class ShowOrders(ListView):
+    model = Order
+    template_name = "show_orders.html"
+    context_object_name = "orders"
+
+    def get_queryset(self, queryset=None):
+        return Order.objects.filter(user=self.request.user, order_status=OrderStatusChoices.ORDER_PLACED)
+
+
 class ShowCart(DetailView):
     model = Order
     template_name = "show_cart.html"
     context_object_name = "order"
 
     def get_object(self, queryset=None):
-        order = get_object_or_404(Order, user=self.request.user)
+        order = get_object_or_404(Order, user=self.request.user, order_status=OrderStatusChoices.IN_CART)
         return order
 
 
