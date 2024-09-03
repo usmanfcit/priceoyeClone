0b2650ab9242bcd955ab6df5db00ffa784d04c9d
usmanfcit <usmanaltaf.work@gmail.com>
2024-09-03 11:05:52 +0500

    Merged all features and removed unnecessary files

--- orders/ticket-api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("list-or-create", views.SupportTicketListCreateAPIView.as_view()),
-    path("update-or-delete/<int:pk>", views.SupportTicketUpdateDestroyAPIView.as_view()),
-]
